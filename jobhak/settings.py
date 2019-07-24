86aac55da321b9af0350a8aff636f7716ef9a717
unknown <akawolf13@syuin.ac.kr>
2019-07-25 02:30:49 +0900

    07.25 mj+yg not css / not layout

--- jobhak/settings.py
+++ jobhak/settings.py
@@ -131,7 +131,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'jobcomment', 'static',)
+    os.path.join(BASE_DIR, 'jobcomment', 'static', )
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'jobhak', 'static', )
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
