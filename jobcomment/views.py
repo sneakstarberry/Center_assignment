3dc43528fbacb33aad4afaf24573e66fbfe7292c
sneakstarberry <gihong2012@likelion.org>
2019-07-25 18:54:47 +0900

    erase backend

--- jobcomment/views.py
+++ jobcomment/views.py
@@ -101,7 +101,7 @@ def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            auth.login(request, user)
             return redirect('home')
     return render(request, 'signup.html')
 
